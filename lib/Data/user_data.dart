556d9eabde08e226966c780a0e812454c930990d
DianRestu <DianRestu13>
2025-09-17 11:59:37 +0700

    first commit

---
+++ lib/Data/user_data.dart
@@ -0,0 +1,10 @@
+/// Variabel global untuk menyimpan data pengguna.
+/// Struktur: Map<String, Map<String, String>>
+/// Key luar adalah email, value adalah Map yang berisi fullName dan password.
+Map<String, Map<String, String>> userData = {
+  // Contoh data awal (bisa dikosongkan)
+  'restukhoirunnisa13@gmail.com': {
+    'fullName': 'Dian Cantikk',
+    'password': '131313',
+  },
+};
